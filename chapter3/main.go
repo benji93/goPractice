d2a321b9ff71698698fba2c0a165d7c2d2f33c9d
benji93 <benjamin.james.fell@gmail.com>
2017-10-06 12:01:42 +0100

    Changes to chapter 3

--- chapter3/main.go
+++ chapter3/main.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt" // Format: used to output code
+	"math" // Math used to find the power of two numbers
+)
 
 func boolean() {
-	fmt.Println(true && true)
+	fmt.Println((true && false) || (false && true) || !(false && false))
 }
 
 func strandint() {
@@ -12,7 +15,30 @@ func strandint() {
 	fmt.Println("Hello " + "world")
 }
 
+// Find the largest number
+func largestNum(num float64) {
+	num = math.Pow(10, num)
+	fmt.Println("You get:", num - 1)
+}
+
+func computeTwoNum(x, y int32) {
+	var num = x * y
+	fmt.Println(num);
+}
+
+func getStringLen(userString string) {
+	var out = len(userString)
+	fmt.Println(out);
+}
+
 func main() {
 	strandint()
+	fmt.Println("-- Boolean Values --")
 	boolean()
+	fmt.Println("-- Largest Number --")
+	largestNum(2)
+	fmt.Println("-- Compute two numbers --")
+	computeTwoNum(32132, 42452)
+	fmt.Println("-- Get string length --")
+	getStringLen("The quick brown fox jummed over the lazy dog")
 }
