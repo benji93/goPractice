c49eb5e007722a7315c83353c8984caf0e36aa38
benji93 <benjamin.james.fell@gmail.com>
2017-10-07 11:34:05 +0100

    Fizz buzz program to test switch statements

--- chapter5/main.go
+++ chapter5/main.go
@@ -16,9 +16,38 @@ func for2() {
 	}
 }
 
+func ifState() {
+	for i := 1; i <= 10; i++ {
+		if i % 2 == 0 {
+			fmt.Println(i, "Even")
+		} else {
+			fmt.Println(i, "Odd")
+		}
+	}
+}
+
+func fizzBuzz() {
+	var input int
+	fmt.Print("Enter a number: ")
+	fmt.Scanf("%d", &input)
+
+	for i := 1; i <= input; i++ {
+		switch {
+			case (i % 3 == 0) && (i % 5 == 0): fmt.Println(i, "FizzBuzz") 
+			case i % 3 == 0: fmt.Println(i, "Fizz")
+			case i % 5 == 0: fmt.Println(i, "Buzz")
+			default: fmt.Println(i)
+		}
+	}
+}
+
 func main() {
 	fmt.Println("-- For1 --")
 	for1()
 	fmt.Println("\n-- For2 --")
 	for2()
+	fmt.Println("\n-- Odd/Even --")
+	ifState()
+	fmt.Println("\n-- FizzBuzz --")
+	fizzBuzz()
 }
