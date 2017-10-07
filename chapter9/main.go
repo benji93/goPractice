37bd7ce96c00e2f171b22b236806d1e1cc6aa340
benji93 <benjamin.james.fell@gmail.com>
2017-10-07 21:04:48 +0100

    Chapter9 Fields

--- chapter9/main.go
+++ chapter9/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
-	// "math"
+	"math"
 )
 
 type Circle struct {
 	x, y, r float64
 }
 
+func circleArea(c *Circle) float64 {
+	return math.Pi * c.r*c.r
+}
+
 func main() {
 	c := Circle{0, 0, 5}
-	fmt.Println(c)
+	fmt.Println(c.r) // Get the value
+	fmt.Println(circleArea(&c))
 }
