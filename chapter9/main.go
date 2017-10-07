47b6e35cede9037afbb6594f3fe43934bae79229
benji93 <benjamin.james.fell@gmail.com>
2017-10-07 21:19:09 +0100

    Chapter 9 - Methods

--- chapter9/main.go
+++ chapter9/main.go
@@ -9,12 +9,31 @@ type Circle struct {
 	x, y, r float64
 }
 
-func circleArea(c *Circle) float64 {
+type Rectangle struct {
+	x1, x2, y1, y2 float64
+}
+
+func (c *Circle) area() float64 {
 	return math.Pi * c.r*c.r
+}
+
+func (r *Rectangle) area() float64 {
+	l := distance(r.x1, r.x2, r.y1, r.y2)
+	w := distance(r.x1, r.x2, r.y1, r.y2)
+	return math.Sqrt(l * w)
+}
+
+func distance(x1, x2, y1, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+	return (a*a + b*b)
 }
 
 func main() {
 	c := Circle{0, 0, 5}
 	fmt.Println(c.r) // Get the value
-	fmt.Println(circleArea(&c))
+	fmt.Println(c.area())
+
+	r := Rectangle{0, 10, 0, 10}
+	fmt.Println(r.area())
 }
