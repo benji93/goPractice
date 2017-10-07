1f0eb4235b24a19526ba7007afc75219fd450994
benji93 <benjamin.james.fell@gmail.com>
2017-10-07 16:02:44 +0100

    Variadic function

--- chapter7/main.go
+++ chapter7/main.go
@@ -10,13 +10,29 @@ func total(xs []float64) float64 {
 	return total
 }
 
+func sum(xs ...float64) float64 {
+	sum := 0.0
+	for _, v := range xs {
+		sum += v
+	}
+	return sum
+}
+
 func average(xs []float64) float64 {
 	return total(xs) / float64(len(xs))
 }
 
+func multi(xs []float64) (float64, float64) {
+	return total(xs), average(xs)
+}
+
 func main() {
 	xs := []float64{98, 32, 77, 82, 89}
-
+	fmt.Println("-- Total and average --")
 	fmt.Println(total(xs))
 	fmt.Println(average(xs))
+	fmt.Println("-- Return multiple values --")
+	fmt.Println(multi(xs))
+	fmt.Println("-- Variadic Function --")
+	fmt.Println(sum(xs...))
 }
