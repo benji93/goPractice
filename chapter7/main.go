60005990bc5039519e01e9e3afebad8cf5603401
benji93 <benjamin.james.fell@gmail.com>
2017-10-07 16:30:26 +0100

    Factorial

--- chapter7/main.go
+++ chapter7/main.go
@@ -26,6 +26,36 @@ func multi(xs []float64) (float64, float64) {
 	return total(xs), average(xs)
 }
 
+func closure() {
+	add := func(x, y int) int {
+		return x + y
+	} 
+	fmt.Println(add(10, 12))
+}
+
+func makeEvenGenerator() func() uint {
+	i := uint(0)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
+	}
+}
+
+func getEvenMain() {
+	nextEven := makeEvenGenerator()
+	fmt.Println(nextEven()) // 0
+	fmt.Println(nextEven()) // 2
+	fmt.Println(nextEven()) // 4
+}
+
+func factorial(x uint) uint {
+	if x == 0 {
+		return 1
+	}
+	return x * factorial(x-1)
+}
+
 func main() {
 	xs := []float64{98, 32, 77, 82, 89}
 	fmt.Println("-- Total and average --")
@@ -35,4 +65,10 @@ func main() {
 	fmt.Println(multi(xs))
 	fmt.Println("-- Variadic Function --")
 	fmt.Println(sum(xs...))
+	fmt.Println("-- Closure / Func within a Func --")
+	closure()
+	fmt.Println("-- Get Even Number --")
+	getEvenMain()
+	fmt.Println("-- Factorial --")
+	fmt.Println(factorial(3))
 }
