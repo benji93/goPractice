ab1fba991544ced4bf357e78682d51e192ea1346
benji93 <benjamin.james.fell@gmail.com>
2017-10-07 20:35:04 +0100

    Final problems

--- chapter8/main.go
+++ chapter8/main.go
@@ -15,16 +15,46 @@ func example1() {
 func zeroPoint(xPtr *int) {
 	*xPtr = 0
 }
+
 func example2() {
 	x := 5
 	zeroPoint(&x)
 	fmt.Println(x) // Will change to zero
 }
 
+func one(xPtr *int) {
+	*xPtr = 1
+}
+
+func newEx() {
+	xPtr := new(int)
+	one(xPtr)
+	fmt.Println(*xPtr)
+}
+
+func swap(x, y *int) {
+	*x, *y = *y, *x
+}
+
+func getAddress(x *int) {
+	fmt.Println(x)
+}
+
 func main() {
-	fmt.Println("-- Pointers --")
-	fmt.Println("Example1")
-	example1()
-	fmt.Println("Example2")
-	example2()
+	// fmt.Println("-- Pointers --")
+	// fmt.Println("Example1")
+	// example1()
+	// fmt.Println("Example2")
+	// example2()
+	// fmt.Println("\n-- New --")
+	// newEx()
+	fmt.Println("\n-- Swap --")
+	x := 12
+	y := 76
+	fmt.Println("X =", x, "Y =", y)
+	swap(&x, &y)
+	fmt.Println("X =", x, "Y =", y)
+	fmt.Println("\n-- Get pointer value --")
+	px := 93
+	getAddress(&px)
 }
