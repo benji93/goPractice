adf30f25e4fc131a2db47a73ffb54d051466902c
benji93 <benjamin.james.fell@gmail.com>
2017-10-10 22:23:35 +0100

    Chapter 13 - Reading files short

--- src/chapter13/main.go
+++ src/chapter13/main.go
@@ -2,30 +2,14 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"io/ioutil"
 )
 
 func main() {
-	file, err := os.Open("test.txt")
-	if err != nil {
-		// Handle the error here
-		return
-	}
-	defer file.Close()
-
-	// get the file size
-	stat, err := file.Stat()
+	bs, err := ioutil.ReadFile("test.txt")
 	if err != nil {
 		return
 	}
-
-	// read the file
-	bs := make([]byte, stat.Size())
-	_, err = file.Read(bs)
-	if err != nil {
-		return
-	}
-
 	str := string(bs)
 	fmt.Println(str)
 }
